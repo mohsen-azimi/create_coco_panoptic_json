7333a2d7fa08c5c7087bd074ddeec878ef5ec627
mohsen-azimi <azimi.eng@gmail.com>
2022-07-16 14:09:18 -0700

    updated

--- step1_create_some_fake_dataset.py
+++ step1_create_some_fake_dataset.py
@@ -5,15 +5,15 @@ import numpy
 height = 480
 width = 640
 
-color1 = (255, 0, 0)  # b
+color1 = (0, 0, 0)  # b
 color2 = (0, 255, 0)  # g
 color3 = (0, 0, 255)  # r
 pixel_array = numpy.full((height, width, 3), color1, dtype=numpy.uint8)
 
 pixel_array[0:100, 0:100, :] = color2
-pixel_array[100:200, 100:200, :] = color3
+pixel_array[110:200, 110:200, :] = color3
 
-for i in range(50):
+for i in range(6):
     cv2.imwrite(f'coco_panoptic/panoptic_train2017/fake_train_{i}.png', pixel_array)  # annotation
     cv2.imwrite(f'coco_panoptic/train2017/fake_train_{i}.jpg', pixel_array)  # data
 
